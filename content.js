f51e0e86f4a7ccd82e6229b3c8317370623d83d0
eman <study.eman@gmail.com>
2020-08-17 13:51:19 +0200

    done

---
+++ content.js
@@ -0,0 +1,2 @@
+// "<all_urls>"
+console.log(setInterval(function(){ if(document.querySelector('.ytp-ad-text.ytp-ad-skip-button-text')){document.querySelector('.ytp-ad-text.ytp-ad-skip-button-text').click();} }, 200))
